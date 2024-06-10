f0a2634f50947f4ceec24b444ba789ecf22f87b6
Mark Gichora <markgichora7@gmail.com>
2024-06-10 13:24:51 +0300

    Conerted the boolean array to 2D array. Added random mine imagesto buttons

--- src/main/java/org/techtrek/logic/MinePlacer.java
+++ src/main/java/org/techtrek/logic/MinePlacer.java
@@ -2,37 +2,43 @@ package org.techtrek.logic;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 import java.util.Random;
 
 public class MinePlacer {
 
     Random random = new Random();
-    int randomNumber;
     int numberOfMines = 10;
-
     ImageIcon img;
 
     public MinePlacer(JButton[] buttons) {
+        Boolean[][] isABtnAMine = new Boolean[9][9];
+        
+        for (int i = 0; i < isABtnAMine.length; i++) {
+            for (int j = 0; j < isABtnAMine[i].length; j++) {
+                isABtnAMine[i][j] = false;
+            }
+        }
 
-        Boolean[] isABtnAMine = new Boolean[81];
-        Arrays.fill(isABtnAMine, false);
-        for (int i = 0; i < numberOfMines; i++) {
-            randomNumber = random.nextInt(81);
-            if (!isABtnAMine[randomNumber]) {
-                isABtnAMine[randomNumber] = true;
+        int minesPlaced = 0;
+        while (minesPlaced < numberOfMines) {
+            int row = random.nextInt(9);
+            int col = random.nextInt(9);
+            if (!isABtnAMine[row][col]) {
+                isABtnAMine[row][col] = true;
+                minesPlaced++;
             }
         }
 
         img = new ImageIcon(getClass().getResource("../assets/referenceImages/smallBomb.jpg"));
+        Image image = img.getImage();
+        Image scaledImage = image.getScaledInstance(buttons[0].getWidth(), buttons[0].getHeight(), Image.SCALE_SMOOTH);
+        img = new ImageIcon(scaledImage);
 
-        for (int i = 0; i < isABtnAMine.length; i++) {
-            if (isABtnAMine[i]) {
-                Image image = img.getImage(); 
-                Image scaledImage = image.getScaledInstance(buttons[i].getWidth(), buttons[i].getHeight(), java.awt.Image.SCALE_SMOOTH); 
-                img = new ImageIcon(scaledImage);  
-
-                buttons[i].setIcon(img);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (isABtnAMine[i][j]) {
+                    buttons[i * 9 + j].setIcon(img);
+                }
             }
         }
     }
