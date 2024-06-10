a544e8b0d67f14595bfa40d901e042ec2da0526f
Mark Gichora <markgichora7@gmail.com>
2024-06-10 12:59:25 +0300

    Added mine image to buttons with mines

--- src/main/java/org/techtrek/logic/MinePlacer.java
+++ src/main/java/org/techtrek/logic/MinePlacer.java
@@ -6,26 +6,34 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class MinePlacer {
+
     Random random = new Random();
     int randomNumber;
     int numberOfMines = 10;
+
+    ImageIcon img;
 
     public MinePlacer(JButton[] buttons) {
 
         Boolean[] isABtnAMine = new Boolean[81];
         Arrays.fill(isABtnAMine, false);
-        for (int i = 0; i < numberOfMines; i++){
+        for (int i = 0; i < numberOfMines; i++) {
             randomNumber = random.nextInt(81);
-            if(!isABtnAMine[randomNumber]){
+            if (!isABtnAMine[randomNumber]) {
                 isABtnAMine[randomNumber] = true;
             }
         }
 
+        img = new ImageIcon(getClass().getResource("../assets/referenceImages/smallBomb.jpg"));
+
         for (int i = 0; i < isABtnAMine.length; i++) {
-            if(isABtnAMine[i]) {
-                buttons[i].setBackground(Color.red);
+            if (isABtnAMine[i]) {
+                Image image = img.getImage(); 
+                Image scaledImage = image.getScaledInstance(buttons[i].getWidth(), buttons[i].getHeight(), java.awt.Image.SCALE_SMOOTH); 
+                img = new ImageIcon(scaledImage);  
+
+                buttons[i].setIcon(img);
             }
         }
     }
 }
-
